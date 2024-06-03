87459b48540b9ffecebe7061f076db2c1145a795
PH3-Digital <philipeoliveira1984@gmail.com>
2024-06-03 11:42:59 -0300

    Aprendizado sobre tipos de variáveis, constantes e cast

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos priitivos
+        //classe string
+        
+        String meuNome = "Philipe Oliveira";
+        double salarioMinimo =2500;
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        // acima foi feito um exemplo de cast
+        // abaixo aprendemos a diferença de variável e constante
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+        final double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75;
+        // Acima vemos um exemplo que precisamos adicionar o termo "final" antes do tipo para que se torne constante
+
+
     }
 }
